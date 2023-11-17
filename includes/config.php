12b3d26901fab855e9c12d4ef49b2652513bceeb
Himan <hmnak1088@gmail.com>
2023-11-17 20:57:29 +0530

    port removal

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,11 @@
 <?php
 $hostname = 'localhost';
-$port = 3310; // Specify the correct port number here
+ // Specify the correct port number here
 $username = 'root';
 $password = '';
 $database = 'undergraduate_db';
 
-$con = mysqli_connect($hostname, $username, $password, $database, $port);
+$con = mysqli_connect($hostname, $username, $password, $database);
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
