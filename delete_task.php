fb9a4c1fb30c281fcd5e2d77af01508488463984
Himan <hmnak1088@gmail.com>
2023-11-24 11:15:30 +0530

    ajax delete update

--- delete_task.php
+++ delete_task.php
@@ -3,8 +3,8 @@ include 'includes/sessions.php';
 
 // Check if the request is a POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Ensure the 'task_id' parameter is set
-    if (isset($_POST['task_id'])) {
+    // Ensure the 'task_id' parameter is set and is a positive integer
+    if (isset($_POST['task_id']) && is_numeric($_POST['task_id']) && $_POST['task_id'] > 0) {
         // Sanitize and get the task ID from the POST data
         $taskId = mysqli_real_escape_string($con, $_POST['task_id']);
 
@@ -13,17 +13,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if (mysqli_query($con, $deleteQuery)) {
             // Return a success message
-            echo 'success';
+            echo json_encode(['status' => 'success']);
         } else {
             // Return an error message
-            echo 'error';
+            echo json_encode(['status' => 'error', 'message' => mysqli_error($con)]);
         }
     } else {
-        // Return an error message if 'task_id' is not set
-        echo 'error';
+        // Return an error message if 'task_id' is not set or is invalid
+        echo json_encode(['status' => 'error', 'message' => 'Invalid task ID']);
     }
 } else {
     // Return an error message for non-POST requests
-    echo 'error';
+    echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
 }
 ?>
