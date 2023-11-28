b9fc6e35a50220d27e4ebfbb244f6ed7aa2951cc
Himan <hmnak1088@gmail.com>
2023-11-28 14:01:06 +0530

    comment error removed

--- save_comment.php
+++ save_comment.php
@@ -1,8 +1,20 @@
 <?php
-include('includes/sessions.php');  // Include your database configuration file
+include('includes/config.php');  // Include your database configuration file
 
+session_start(); // Starting Session
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// Check if the user is logged in
+if (isset($_SESSION['login_user'])) {
+
+  // Storing Session
+  $user_check = $_SESSION['login_user'];
+
+  // SQL Query To Fetch Complete Information Of User
+  $ses_sql = mysqli_query($con, "select * from users where username='$user_check'");
+  $row = mysqli_fetch_assoc($ses_sql);
+  $login_id = $row['user_id'];
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $commentText = mysqli_real_escape_string($con, $_POST["commentText"]);
     $userId = $login_id;  // Assuming you have a user_id in the session
     $provinceId = isset($_GET['id']) ? (int)$_GET['id'] : 0;  // Retrieve the province ID from the URL
@@ -30,4 +42,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error: " . $insertQuery . "<br>" . mysqli_error($con);
     }
 }
+
+}
+  else{
+    mysqli_close($con); // Closing Connection
+    header("Location: login_page.php");
+    exit();
+  }
+
 ?>
