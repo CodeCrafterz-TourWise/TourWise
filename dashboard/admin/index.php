5ca793541af7972900dac256413424eaa36617be
Himan <hmnak1088@gmail.com>
2023-11-29 13:32:44 +0530

    event error removal

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -1,6 +1,5 @@
 <?php
 require_once('../../includes/sessions.php');
-
 ?>
 
 <!DOCTYPE html>
@@ -11,8 +10,6 @@ require_once('../../includes/sessions.php');
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
   <title>AdminPanel- TourWise</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
 
   <!-- Favicons -->
   <link href="../../assets/img/Logo.png" rel="icon">
@@ -25,11 +22,6 @@ require_once('../../includes/sessions.php');
   <!-- Vendor CSS Files -->
   <link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="../../assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="../../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="../../assets/vendor/simple-datatables/style.css" rel="stylesheet">
   <link href="../../assets/css/style.css" rel="stylesheet">
 
 </head>
@@ -38,7 +30,6 @@ require_once('../../includes/sessions.php');
 
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center">
-
     <div class="d-flex align-items-center justify-content-between">
       <a href="../../index.php" class="logo d-flex align-items-center">
         <img src="../../assets/img/logo.png" alt="">
@@ -49,10 +40,7 @@ require_once('../../includes/sessions.php');
 
     <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
-
-
         <li class="nav-item dropdown pe-3">
-
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <i class="bi bi-person-circle"></i>
             <span class="d-none d-md-block dropdown-toggle ps-2"><?php echo "$login_session" ?></span>
@@ -74,20 +62,15 @@ require_once('../../includes/sessions.php');
                 <span>Sign Out</span>
               </a>
             </li>
-
           </ul><!-- End Profile Dropdown Items -->
         </li><!-- End Profile Nav -->
-
       </ul>
     </nav><!-- End Icons Navigation -->
-
   </header><!-- End Header -->
 
   <!-- ======= Sidebar ======= -->
   <aside id="sidebar" class="sidebar">
-
     <ul class="sidebar-nav" id="sidebar-nav">
-
       <li class="nav-item">
         <a class="nav-link " href="index.php">
           <i class="bi bi-grid"></i>
@@ -104,17 +87,18 @@ require_once('../../includes/sessions.php');
           <i class="bi bi-layout-text-window-reverse"></i><span>Profile</span>
         </a>
       </li>
-  </aside><!-- End Sidebar-->
+    </ul><!-- End Sidebar-->
+  </aside>
 
   <main id="main" class="main">
-
     <div class="pagetitle">
       <h1>Dashboard</h1>
     </div><!-- End Page Title -->
+
     <!-- ======= Event Update Form ======= -->
     <div class="container mt-5">
       <h2>Event Update</h2>
-      <form action="event_update.php" method="post">
+      <form action="post_event.php" method="post">
         <div class="mb-3">
           <label for="eventTitle" class="form-label">Event Title</label>
           <input type="text" class="form-control" id="eventTitle" name="eventTitle" required>
@@ -127,52 +111,45 @@ require_once('../../includes/sessions.php');
         <input type="hidden" name="adminId" value="<?php echo $login_id; ?>">
         <button type="submit" class="btn btn-primary">Post Update</button>
       </form>
+
       <?php
       // Retrieve and display event updates
       $selectQuery = "SELECT * FROM events ORDER BY event_id DESC"; // Order by the most recent event first
       $result = mysqli_query($con, $selectQuery);
 
       if (mysqli_num_rows($result) > 0) {
-          while ($row = mysqli_fetch_assoc($result)) {
-              $eventId = $row['event_id'];
-              $eventTitle = $row['title'];
-              $eventDescription = $row['description'];
+        while ($row = mysqli_fetch_assoc($result)) {
+          $eventId = $row['event_id'];
+          $eventTitle = $row['title'];
+          $eventDescription = $row['description'];
 
-              // Display event update with a delete option
-              echo '<div class="mt-4">';
-              echo "<h3>$eventTitle</h3>";
-              echo "<p>$eventDescription</p>";
+          // Display event update with a delete option
+          echo '<div class="mt-4">';
+          echo "<h3>$eventTitle</h3>";
+          echo "<p>$eventDescription</p>";
 
-              // Check if the user is authorized to delete
-              if ($login_id == $row['admin_id']) {
-                  echo '<form action="delete_event.php" method="post">';
-                  echo '<input type="hidden" name="eventId" value="' . $eventId . '">';
-                  echo '<button type="submit" class="btn btn-danger">Delete</button>';
-                  echo '</form>';
-              }
-
-              echo '</div>';
+          // Check if the user is authorized to delete
+          if ($login_id == $row['admin_id']) {
+            echo '<form action="delete_event.php" method="post">';
+            echo '<input type="hidden" name="eventId" value="' . $eventId . '">';
+            echo '<button type="submit" class="btn btn-danger">Delete</button>';
+            echo '</form>';
           }
+
+          echo '</div>';
+        }
       } else {
-          echo '<p>No event updates available.</p>';
+        echo '<p>No event updates available.</p>';
       }
       ?>
-  </div>
+    </div>
   </main><!-- End #main -->
-
-
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="../../assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../../assets/vendor/chart.js/chart.umd.js"></script>
-  <script src="../../assets/vendor/echarts/echarts.min.js"></script>
-  <script src="../../assets/vendor/quill/quill.min.js"></script>
   <script src="../../assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="../../assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="../../assets/vendor/php-email-form/validate.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
   <!-- Template Main JS File -->
